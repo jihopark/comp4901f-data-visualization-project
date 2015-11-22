d80f6c8a4452d57c717ecbd9629941a34f8430d8
jihopark <lastspurt17@gmail.com>
2015-11-22 15:10:41 +0800

    Done with Data counting

--- heatmap.js
+++ heatmap.js
@@ -1,5 +1,6 @@
 (function() {
 
+
   var data = {};
 
   function loadData(day, data) {
@@ -9,6 +10,7 @@
       d3.csv("/data/comm-data-Fri.csv", function(loaded) {
         data[day] = loaded;
         console.log("Data Load Done. Total Row:" + data[day].length);
+        countData(data[day]);
       });
     }
     else {
@@ -16,6 +18,54 @@
     }
   };
 
-  loadData("Sat", data);
+  function countData(data) {
+    var locationMap = {};
+
+    //Sort data according to location
+    data.forEach(function(row){
+      var foundLocation = false;
+      for (var i=0, locationNum=Object.keys(locationMap).length ; i<locationNum; i++) {
+        var location = Object.keys(locationMap)[i];
+        if (location == row["location"]) {
+          foundLocation = true;
+          locationMap[location].push(row);
+          break;
+        }
+      }
+      //If Location is new
+      if (!foundLocation) {
+        locationMap[row["location"]] = [ ];
+        locationMap[row["location"]].push(row);
+      }
+    });
+    for (var i=0, locationNum=Object.keys(locationMap).length ; i<locationNum; i++) {
+      var key = Object.keys(locationMap)[i];
+      locationMap[key] = countTimeInterval(locationMap[key]);
+    }
+  }
+
+  function countTimeInterval(data) {
+    var count = [];
+    var start = d3.time.day(new Date(data[0]["Timestamp"]));
+    var timeInterval = d3.time.minute.range(d3.time.hour.offset(start, 8), d3.time.minute.offset(start, 1470), 30);
+
+    for (var i=0;i<timeInterval.length-1;i++) {
+      count[i] = 0;
+    }
+
+    var intervalStart, intervalEnd;
+    for (var i=0;i<data.length;i++){
+      for (var j=0;j<timeInterval.length-1;j++) {
+        intervalStart = timeInterval[j];
+        intervalEnd = timeInterval[j+1];
+        var time = new Date(data[i]["Timestamp"]);
+        if (time >= intervalStart && time <= intervalEnd)
+          count[j]++;
+      }
+    }
+    return count;
+  }
+
+  loadData("Fri", data);
 
 })();
