c4b1d62d2f50c39c2765cfc47ca5e3095ab9b792
jihopark <lastspurt17@gmail.com>
2015-11-22 13:00:33 +0800

    Added Load Data

--- heatmap.js
+++ heatmap.js
@@ -1,6 +1,21 @@
 (function() {
-  d3.csv("/data/comm-data-Fri.csv", function(data) {
-    alert(data[0]["location"]);
-  });
+
+  var data = {};
+
+  function loadData(day, data) {
+    var fileName = "/data/comm-data-" + day + ".csv";
+    if (!data[day]) {
+      console.log("Need to load data for " + day);
+      d3.csv("/data/comm-data-Fri.csv", function(loaded) {
+        data[day] = loaded;
+        console.log("Data Load Done. Total Row:" + data[day].length);
+      });
+    }
+    else {
+      console.log("Already Loaded");
+    }
+  };
+
+  loadData("Sat", data);
 
 })();
